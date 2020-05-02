f305c6db74b00e52300ff0374e6736baccb4cac9
Tim Retzlaff <retzlaff-tim@gmx.de>
2020-05-02 17:47:58 +0200

    Integrated a REST API interface.

---
+++ database/tweets.js
@@ -0,0 +1,61 @@
+// ----- Required Modules -----
+
+const Tweet = require('./models/Tweet');
+
+// Common Functions
+const common = require('../common/common');
+
+
+// ----- Public Functions -----
+
+function Store(tweets)
+{
+    if (!tweets)
+        return;
+
+    tweets.forEach(tweet =>
+    {
+        Tweet.findOne({ "tweet.id" : tweet.id })
+            .then(async tw =>
+            {
+                // Tweet doesnt't exist in database
+                if (!tw)
+                {
+                    tw = new Tweet
+                    (
+                        {
+                            tweet
+                        }
+                    );
+
+                    tw.save().then(t =>
+                    {
+                        common.Log('Info', `Tweet (${t.id}) saved to database`);
+                    })
+                    .catch(err => common.Log('Database error', err));
+                }
+                else
+                {
+                    tw.save().then(t =>
+                    {
+                        common.Log('Info', `Tweet (${t.id}) updated`);
+                    })
+                    .catch(err => common.Log('Database error', err));
+                }
+            });
+    });
+}
+
+function DeleteAll()
+{
+    Tweet.deleteMany({}).then(x => common.Log('Database',  'Deleted all documents from tweet collection')).catch(err => common.Log('Database error',  err));
+}
+
+
+// ------ Export Modul ------
+
+module.exports =
+{
+    Store,
+    DeleteAll
+};
